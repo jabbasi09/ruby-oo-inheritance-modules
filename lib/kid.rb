b7f868ca84ca445f62bdb0fa2e1c7f65421233df
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-11-18 20:08:09 -0600

    commit tree 54588667b286c395c9e75112c5cccc8a04642b1d

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+    # To lend our two classes all of the methods of the Dance module, we use the include keyword:
+    include Dance
+    extend MetaDancing
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
